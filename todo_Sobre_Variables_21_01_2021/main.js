e8dab1bfe847258c4599784e641f8650bd670364
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-01-25 19:16:44 -0500

    Taller1_21_01_2021

---
+++ todo_Sobre_Variables_21_01_2021/main.js
@@ -0,0 +1,10 @@
+let nombre = "Miguel Angel Castro Escamilla";
+let edad = 23;
+let soy_profesor = true;
+const datos = {id: 123456789, dirrecion: "Calle 11"};
+
+
+console.log("Nombre ", typeof(nombre), nombre);
+console.log("Edad ", typeof(edad), edad);
+console.log("Soy profesor ", typeof(soy_profesor), soy_profesor);
+console.log("Informacion ", typeof(datos), datos);
